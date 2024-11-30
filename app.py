52e315be5dc36ae9bdd2b85771f85b196377e3da
erick-hualla-code <erick.hualla.code@gmail.com>
2024-11-30 03:38:18 -0500

    Feat: Client requirement endpoint added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from controllers.generate_team_members import generate_team_members
 from controllers.generate_tech_stacks import generate_tech_stacks
 from controllers.generate_team_member_experiences import generate_team_member_experiences
 from controllers.generate_experience_units import generate_experience_units
+from controllers.generate_client_requirements import generate_client_requirements
 
 load_dotenv()
 
@@ -37,6 +38,10 @@ def generate_team_member_experiences_controller():
 def generate_experience_units_controller():
   return generate_experience_units()
 
+
+@app.post("/api/generate-clients-requirements")
+def generate_client_requirements_controller():
+  return generate_client_requirements()
 
 if __name__ == '__main__':
   app.run(
---
+++ controllers/generate_client_requirements.py
@@ -0,0 +1,30 @@
+import json
+from flask import jsonify
+from utils.convert_enum_to_list import convert_enum_to_list
+from constants.tech_stack import TechStack
+from utils.openai_client import openai_client
+
+def get_client_requirements_prompt():
+  tech_stack_list = convert_enum_to_list(TechStack)
+  tech_stack_formatted = ", ".join(tech_stack_list)
+  
+  return f"""
+  You are in charge on generating a complete requirements text for a random project provided from a fictional client consulting a software development team.
+  The requirements must follow these rules:
+  * Include a detailed functionality and the exactly techstack used for it (only one per functionalitie).
+  * The selected techstack must strictly be inside this provided list: {tech_stack_formatted}.
+  * Each techstack can appear multiple times but in different functionalities.
+  
+  You should return a JSON text contained in a sigle element (one paragraph) cointaining the mutiples vi
+  """
+  
+def generate_client_requirements():
+    chat_completion = openai_client.chat.completions.create(
+    model="gpt-4o",
+    response_format={"type": "json_object"},
+    messages=[
+        {"role": "user", "content": get_client_requirements_prompt}]
+    )
+    
+    response = chat_completion.choices[0].message.content
+    return jsonify({"requirements": response})
